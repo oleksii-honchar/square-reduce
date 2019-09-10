a393ee4aaf1e0752f4c817e18d9eb0dd5c3338a6
olho <olho@ciklum.com>
2019-09-10 12:07:08 +0300

    * wip 1

--- src/Square.ts
+++ src/Square.ts
@@ -27,13 +27,32 @@ export class Square {
   merge(square: Square) {
     const noChildren = !this.hasChildren() && !square.hasChildren();
     const isSomeoneWhite = this.isWhite() || square.isWhite();
+
+    // someone white will dominate
     if (noChildren || isSomeoneWhite) {
       return new Square([],this.isWhite() || square.isWhite());
     }
 
-    let children = this.hasChildren() ? this.children : square.getChildren();
+    // someone's children will override simple black
+    const isSomeoneBlack = !this.isWhite() || !square.isWhite();
+    if (!noChildren && isSomeoneBlack) {
+      const children = this.hasChildren() ? this.children : square.getChildren();
+      return new Square(children);
+    }
+
+    // let's merge
+    let children = [];
+    let mergeWith = null;
+    if (this.hasChildren()) {
+      children = this.children;
+      mergeWith = square
+    } else {
+      children = square.getChildren();
+      mergeWith = this;
+    }
+
     children = children.map(
-      (childSquare: Square, idx) => childSquare.merge(square.getChildByIdx(idx))
+      (childSquare: Square, idx) => childSquare.merge(mergeWith.getChildByIdx(idx))
     );
 
     return new Square(children);
